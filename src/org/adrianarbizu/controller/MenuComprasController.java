f003e7fd30a499e637f87f818907a0724596beb3
aarbizu-2020398 <aarbizu-2020398@kinal.edu.gt>
2024-05-25 00:29:18 -0600

    Entrega final del proyecto

---
+++ src/org/adrianarbizu/controller/MenuComprasController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.adrianarbizu.controller;
+
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.adrianarbizu.system.Main;
+
+public class MenuComprasController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Supermarket Shopping Menu");
+
+        Label productNameLabel = new Label("Product Name:");
+        TextField productNameTextField = new TextField();
+
+        Label productQuantityLabel = new Label("Product Quantity:");
+        TextField productQuantityTextField = new TextField();
+
+        Button addButton = new Button("Add to Cart");
+        addButton.setOnAction(event -> {
+        
+            System.out.println("Product added to cart!");
+        });
+
+        Button viewCartButton = new Button("View Cart");
+        viewCartButton.setOnAction(event -> {
+            System.out.println("Viewing cart...");
+        });
+
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(10));
+        root.getChildren().addAll(productNameLabel, productNameTextField, productQuantityLabel, productQuantityTextField, addButton, viewCartButton);
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void setStage(Main aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
